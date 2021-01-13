703e450a85d515501053a9e559fcc74545943544
maxjacobzander <maxjacobzander@gmail.com>
2021-01-13 12:24:40 -0500

    added activerecord validations for presence of user (name and username) and liquor (name and variety)

--- app/models/liquor.rb
+++ app/models/liquor.rb
@@ -1,3 +1,5 @@
 class Liquor < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :variety, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :name, presence: true
+    validates :username, uniqueness: true, presence: true
 end
