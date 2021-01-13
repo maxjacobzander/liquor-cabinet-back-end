b6f9cc7b9f81e8c5fa9509e16f7c3517c28574c7
maxjacobzander <maxjacobzander@gmail.com>
2021-01-13 11:51:09 -0500

    migrated liquors and added birthday to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_162348) do
+ActiveRecord::Schema.define(version: 2021_01_13_164928) do
+
+  create_table "liquors", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.integer "year"
+    t.string "origin"
+    t.text "tasting_notes"
+    t.string "image"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_liquors_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +31,8 @@ ActiveRecord::Schema.define(version: 2021_01_13_162348) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "birthday"
   end
 
+  add_foreign_key "liquors", "users"
 end
