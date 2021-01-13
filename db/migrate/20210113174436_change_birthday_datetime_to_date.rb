5f32fcde7b0df35540837ccf95bdac74e94e9c4c
maxjacobzander <maxjacobzander@gmail.com>
2021-01-13 12:45:50 -0500

    changed birthday datetime to date

---
+++ db/migrate/20210113174436_change_birthday_datetime_to_date.rb
@@ -0,0 +1,5 @@
+class ChangeBirthdayDatetimeToDate < ActiveRecord::Migration[6.0]
+  def change
+    change_column :users, :birthday, :date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_170826) do
+ActiveRecord::Schema.define(version: 2021_01_13_174436) do
 
   create_table "liquors", force: :cascade do |t|
     t.string "name"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2021_01_13_170826) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.datetime "birthday"
+    t.date "birthday"
   end
 
   add_foreign_key "liquors", "users"
