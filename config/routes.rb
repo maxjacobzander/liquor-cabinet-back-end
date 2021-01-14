57fa4b2892684eabff5f877084b5361e39dc5276
maxjacobzander <maxjacobzander@gmail.com>
2021-01-14 09:52:08 -0500

    reintegrated namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  resources :liquors
-  resources :users do
-    resources :liquors
+  namespace :api do
+    namespace :v1 do
+      resources :liquors
+      resources :users do
+        resources :liquors
+      end
+    end
   end
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :users do
-  #       resources :liquors
-  #     end
-  #   end
-  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
