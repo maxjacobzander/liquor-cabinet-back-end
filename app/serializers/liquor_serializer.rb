e3b4c6c4515d588e9a12835c50821e29647a2de7
maxjacobzander <maxjacobzander@gmail.com>
2021-01-14 10:48:58 -0500

    migrated liquor rating

--- app/serializers/liquor_serializer.rb
+++ app/serializers/liquor_serializer.rb
@@ -1,5 +1,5 @@
 class LiquorSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :variety, :year, :origin, :tasting_notes, :image
+  attributes :name, :variety, :year, :origin, :tasting_notes, :rating, :image
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_174436) do
+ActiveRecord::Schema.define(version: 2021_01_14_154702) do
 
   create_table "liquors", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_01_13_174436) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "rating"
     t.index ["user_id"], name: "index_liquors_on_user_id"
   end
 
