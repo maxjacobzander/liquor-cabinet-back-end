9cb22bfd5fb360cf4ffc11a58d5421bcce7fd14f
maxjacobzander <maxjacobzander@gmail.com>
2021-01-14 07:42:33 -0500

    added helpers for mocked current_user and logged_in? methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+    helpers do
+        def current_user
+            # mocked up logged in
+            User.first
+        end
+        def logged_in?
+            !!current_user
+        end
+    end
 end
