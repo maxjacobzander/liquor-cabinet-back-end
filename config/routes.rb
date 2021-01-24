a536637bb2155c0bf58fb7e0eae813ac5cec3555
maxjacobzander <maxjacobzander@gmail.com>
2021-01-24 00:13:10 -0500

    added POST to search

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     end
   end
   get "/api/v1/search", to: "api/v1/drinks#search"
+  post "/api/v1/search", to: "api/v1/drinks#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
